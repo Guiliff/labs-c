4184cc6845705c1abc2e602fd028adb87af29b31
Guilherme Licodiedoff <guiliff@protonmail.com>
2025-05-20 06:32:28 -0300

    Update double_float.c

--- double_float.c
+++ double_float.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Este código mostra a diferença de precisão entre o float e double.
+// This program demonstrates the difference in precision between float and double
+
 int main(){
     float number1 = 1.102030405060;
     double number2 = 1.102030405060;
