a91e470c07c6042e3e3ea4811b54df6c46261410
Guilherme Licodiedoff <guiliff@protonmail.com>
2025-05-20 06:29:06 -0300

    Add files via upload

---
+++ double_float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float number1 = 1.102030405060;
+    double number2 = 1.102030405060;
+
+    printf("\nFloat = %.10f\n", number1);
+    printf("\nDouble = %.10f\n\n", number2);
+
+}
