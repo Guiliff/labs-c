ef3ee784f75759f0b716328ddbc5fbba86537a2b
Guilherme Licodiedoff <guiliff@protonmail.com>
2025-05-28 06:44:54 -0300

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+void welcome(){
+    printf("\n--------------------------------\n");
+    printf("            HANGMAN  \n");
+    printf("--------------------------------\n");
+}
+
+void shot(){
+    char guess;
+    scanf(" %c", &guess); //Correcting a buffer error here
+    guess2 [attempts] = guess;
+    attempts++;
+}
+
 int main(){
     char sw[20];
     sprintf(sw, "HAWKMAN");
@@ -12,11 +25,15 @@ int main(){
     char guess2 [26];
     int attempts = 0;
 
+    welcome();
+
     do{
+        //imprime a palavra secreta
         for (int i=0; i<strlen(sw); i++){
             
             int found = 0;
 
+            //a letra já foi chutada?
             for(int j=0; j<attempts; j++){
                 if(guess2[j] == sw[i]){
                     found = 1;
@@ -30,12 +47,8 @@ int main(){
             }
         }
         printf("\n");
-
-        char guess;
-        scanf(" %c", &guess); //Correcting a buffer error here
-
-        guess2 [attempts] = guess;
-        attempts++;
+        shot();
 
     } while(!correct && !incorrect);
+    return 0;
 }
